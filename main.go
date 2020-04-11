d538ad187e45e512c84dced8e0f95874fdb13d71
zhaobin.szb <zhaobin.szb@alibaba-inc.com>
2020-04-11 18:32:30 +0800

    move libs inside src

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ package main
 
 // import "github.com/nysanier/nygosdk1/lib"
 import (
-	"github.com/nysanier/nygosdk1/nylib" // gopath 指向当前repo, 否则就需要用下面的相对路径了
+	"nylib" // gopath 指向当前repo, 否则就需要用下面的相对路径了
 	// "./nylib"
 	// "./nylib2/sub2"
-	"github.com/nysanier/nygosdk1/nylib2/sub2"
+	"nylib2/sub2" // go get 的时候如何依赖本地包?
 )
 
 func main() {
